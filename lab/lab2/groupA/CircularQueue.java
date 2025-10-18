142412302aa42eae357b7ef1927d8035b6b097ae
60Hukuma92 <114644174+60Hukuma92@users.noreply.github.com>
2025-10-18 14:24:01 +0300

    Update CircularQueue.java
    CircularQueue is now overwriting its elements when size == CAPACITY. Now it does not throw IlligalStateException when it is full.

--- lab/lab2/groupA/CircularQueue.java
+++ lab/lab2/groupA/CircularQueue.java
@@ -18,28 +18,19 @@ public class CircularQueue<T> {
     }
 
     public void enqueue(T element) {
-        /*CircularQueue - СД, основанная на массиве. Её цикличность позволяет записывать элементы в конец очереди
-        фактически в начало массива, если там освободилось место, то есть некоторые элементы были удалены из очереди.
-        При этом цикличность - это только про реализацию очереди, а не про функционал. Он совпадает с простым Queue.*/
-        if (isFull()) throw new IllegalStateException("CircularQueue is overloaded");
-        rear = (rear + 1) % CAPACITY;   /*Круговое (циклическое) перемещение указателя на конец очереди
-                                        возвращаемся к началу (0). Гарантирует, что индекс всегда остается в границах [0, CAPACITY-1].
-                                        %CAPACITY при перемещении указателя rear обеспечивает то, что после достижения индекса CAPACITY-1,
-                                        в тот момент, когда rear должен стать равным CAPACITY, остаток от деления перемещает указатель на индекс 0,
-                                        ведь CAPACITY % CAPACITY даёт 0. Всем индексам, меньшим CAPACITY, а именно такими они и являются,
-                                        %CAPACITY не мешает, ведь он даёт именно эти же индексы.
-                                        */
-        queue[rear] = element;
-        size++;
+    if (isFull()) {
+        dequeue(); // Удаляем старый элемент чтобы освободить место
+    }
+    rear = (rear + 1) % CAPACITY;
+    queue[rear] = element;
+    size++;
     }
 
     public T dequeue() {
         if (isEmpty()) throw new IllegalStateException("Queue is empty");
         T removedElement = queue[front];
         queue[front] = null;
-        front = (front + 1) % CAPACITY; /*Циклическое перемещение указателя на начало очереди
-                                         При достижении конца массива указатель переходит к 0.
-                                         Все пояснения аналогичны пояснению выше для сдвига указателя rear*/
+        front = (front + 1) % CAPACITY;
         size--;
         return removedElement;
     }
@@ -67,11 +58,7 @@ public class CircularQueue<T> {
         int count = 0;
         while (count++ < size - 1) {
             System.out.print(queue[i] + ", ");
-            i = (i + 1) % CAPACITY; /*Циклическое (круговое) перемещение индекса
-                                     Циклический обход от front к rear.
-                                     Обеспечивает корректный переход через границу массива
-                                     Правило циклического обхода остаётся тем же.
-                                     Учитывается случай rear < front как раз при круговом обходе.*/
+            i = (i + 1) % CAPACITY;
         }
         if (!isEmpty()) System.out.println(queue[i] + "]");
         else System.out.println("]");
@@ -83,10 +70,7 @@ public class CircularQueue<T> {
         int count = 0;
         while (count++ < size - 1) {
             System.out.print(queue[i] + ", ");
-            i = (i - 1 + CAPACITY) % CAPACITY;  /*Циклический обход в обратном направлении.
-                                                +CAPACITY предотвращает отрицательные индексы,
-                                                затем % CAPACITY обеспечивает зацикливание.
-                                                То есть всё абсолютно аналогично пояснению выше. Также учтён случай rear < front*/
+            i = (i - 1 + CAPACITY) % CAPACITY;
         }
         if (!isEmpty()) System.out.println(queue[i] + "]");
         else System.out.println("]");
