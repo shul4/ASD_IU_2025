87bccacfb8db7ec485b3c53245825b53ef5f9cd3
yaneoks24-stack <yaneoks24@gmail.com>
2025-10-18 15:30:57 +0300

    Кашникова ИУ3-31б исправление

--- lab/lab1/Task2Main.java
+++ lab/lab1/Task2Main.java
@@ -20,14 +20,8 @@ public class Task2Main {
     }
 
     public static int romanToArabic(String roman) {
-        Map<Character, Integer> romanValues = new HashMap<>();
-        romanValues.put('I', 1);
-        romanValues.put('V', 5);
-        romanValues.put('X', 10);
-        romanValues.put('L', 50);
-        romanValues.put('C', 100);
-        romanValues.put('D', 500);
-        romanValues.put('M', 1000);
+        Map<Character, Integer> romanValues = createRomanMap();
+
         int result = 0;
         for (int i = 0; i < roman.length(); i++) {
             int current = romanValues.get(roman.charAt(i));
@@ -45,4 +39,15 @@ public class Task2Main {
         return result;
     }
 
+    private static Map<Character, Integer> createRomanMap() {
+        Map<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+        return romanValues;
+    }
 }
