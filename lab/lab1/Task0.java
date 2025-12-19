3a2658bb90ff4bd83552af01eb1375b65e8bd602
shul4 <sulgin.aleksandr7@gmail.com>
2025-12-19 18:39:30 +0300

    Update Task0.java

--- lab/lab1/Task0.java
+++ lab/lab1/Task0.java
@@ -1,59 +1,65 @@
 import java.util.Scanner;
-
+//Пусть любое число – это массив его цифр слева направо. Пример, число
+//1234 – это массив [1,2,3,4].
+//Дан массив целых чисел. Реализовать умножение двух чисел.
+//Пример, [1, 2, 3, 4] * [1, 1] = [1, 3, 5, 7, 4].
+//Результат – число, представленное массивом.
 public class Task0 {
-    public static int binarySearchIterative(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+    public static int[] multiplyArrays(int[] num1, int[] num2) {
+        int n1 = num1.length;
+        int n2 = num2.length;
+        int[] result = new int[n1 + n2];
 
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        for (int i = n1 - 1; i >= 0; i--) {
+            int carry = 0;
+            for (int j = n2 - 1; j >= 0; j--) {
+                int sum = num1[i] * num2[j] + result[i + j + 1] + carry;
+                result[i + j + 1] = sum % 10;
+                carry = sum / 10;
             }
+            result[i] += carry;
         }
-        return -1;
+
+        int start = 0;
+        while (start < result.length - 1 && result[start] == 0) {
+            start++;
+        }
+
+        int[] trimmedResult = new int[result.length - start];
+        for (int i = start; i < result.length; i++) {
+            trimmedResult[i - start] = result[i];
+        }
+
+        return trimmedResult;
     }
 
-    public static int binarySearchRecursive(int[] arr, int target, int left, int right) {
-        if (left > right) {
-            return -1;
+    private static int[] stringToDigitsArray(String numberStr) {
+        int[] digits = new int[numberStr.length()];
+        for (int i = 0; i < numberStr.length(); i++) {
+            digits[i] = numberStr.charAt(i) - '0';
         }
-
-        int mid = (left + right) / 2;
-
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            return binarySearchRecursive(arr, target, mid + 1, right);
-        } else {
-            return binarySearchRecursive(arr, target, left, mid - 1);
-        }
+        return digits;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите количество элементов массива: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        System.out.println("Введите первое число: ");
+        String firstNumberStr = scanner.nextLine();
 
-        System.out.println("Введите отсортированный массив:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        System.out.println("Введите второе число: ");
+        String secondNumberStr = scanner.nextLine();
+
+        int[] number1 = stringToDigitsArray(firstNumberStr);
+        int[] number2 = stringToDigitsArray(secondNumberStr);
+
+        int[] product = multiplyArrays(number1, number2);
+
+        System.out.print("Результат умножения: ");
+        for (int digit : product) {
+            System.out.print(digit);
         }
-
-        System.out.print("Введите число для поиска: ");
-        int target = sc.nextInt();
-
-        int indexIter = binarySearchIterative(arr, target);
-        int indexRec = binarySearchRecursive(arr, target, 0, arr.length - 1);
-
-        System.out.println("Итеративный поиск: " + indexIter);
-        System.out.println("Рекурсивный поиск: " + indexRec);
+        System.out.println();
     }
 }
